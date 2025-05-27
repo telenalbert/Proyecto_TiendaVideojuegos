5d495705e9e7e816fb5e77e24888eab72b12adb1
talberts <trinidad.albert98@gmail.com>
2025-05-27 18:20:40 +0200

    creacion tabla intermedia

--- models/products.js
+++ models/products.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Products.belongsToMany(models.Category);
       // define association here
     }
   }
--- models/products_category.js
+++ models/products_category.js
@@ -3,23 +3,21 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
+  class Products_Category extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Category.belongsToMany(models.Product);
       // define association here
     }
   }
-  Category.init({
-    name: DataTypes.STRING,
+  Products_Category.init({
     description: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Category',
+    modelName: 'Products_Category',
   });
-  return Category;
+  return Products_Category;
 };
