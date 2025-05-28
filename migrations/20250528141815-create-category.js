d608576f824188137466fb046d1d3b326d378ea6
talberts <trinidad.albert98@gmail.com>
2025-05-28 16:22:54 +0200

    Creacion Tablas

---
+++ migrations/20250528141815-create-category.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name_category: {
+        type: Sequelize.STRING
+      },
+      description_category: {
+        type: Sequelize.STRING
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Categories');
+  }
+};
