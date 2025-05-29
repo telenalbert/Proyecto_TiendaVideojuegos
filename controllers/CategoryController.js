9b6e8d2e9bf10ff6288cdda19ee6d38e5e3b4942
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-05-29 20:10:02 +0200

    Categories endpoints parte_bea

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,15 +1,78 @@
-const {Category} = require('../models/index.js');
+//METODO CREATE
+const { Category } = require('../models/index.js');
 
-const CategoryController =  {
+
+const CategoryController = {
     async create(req, res) {
         try {
             const category = await Category.create(req.body);
-            res.status(201).send ({msg: 'Category created', Category});
-        } catch(error) {
-            console.log("Error al crear producto", error);
-            res.status(500).send({error: 'Error when creating category', error})
+            res.status(201).send({ msg: 'Category created', category });
+        } catch (error) {
+            console.log("Error al crear la categoría", error);
+            res.status(500).send({ error: 'Error when creating category', error });
+        }
+    },
+
+    async update(req, res) {
+        try {
+            await Category.update(req.body, {
+                where: { id: req.params.id }
+            });
+            res.status(200).send({ msg: "Categoría actualizada con éxito" });
+        } catch (error) {
+            res.status(500).send({
+                msg: "La categoría no se ha podido actualizar",
+                error: error.message
+            });
+        }
+    },
+
+    async delete(req, res) {
+        try {
+            await Category.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+            res.send("La categoría se ha eliminado");
+        } catch (error) {
+            res.status(500).send({
+                msg: "La categoría no se pudo eliminar",
+                error: error.message
+            });
+        }
+    },
+
+    async getById(req, res) {
+        try {
+            const category = await Category.findByPk(req.params.id);
+            res.status(200).send(category);
+        } catch (error) {
+            res.status(500).send({
+                msg: "No se ha podido cargar la categoría",
+                error: error.message
+            });
+        }
+    },
+
+    async getByName(req, res) {
+        try {
+            const { name } = req.query;
+
+            const category = await Category.findOne({
+                where: {
+                    name_category: name
+                }
+            });
+
+            res.status(200).send(category);
+        } catch (error) {
+            res.status(500).send({
+                msg: "Error al buscar la categoría"
+            });
         }
     }
-}
+};
 
-module.exports = CategoryController
+
+module.exports = CategoryController;
--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/CategoryController')
 
-router.post('/',CategoryController.create)
+router.post('/',CategoryController.create);
+router.put('/:id',CategoryController.update);
+router.delete('/:id', CategoryController.delete);
+router.get('/:id', CategoryController.getById);
 
 module.exports = router;
